10fe8b703709096f768601148e167cc0a9f4e787
anandhunv <anandhunv05@gmail.com>
2025-03-22 23:43:24 +0530

    package.json issue

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,10 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes); // Add User Routes
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
